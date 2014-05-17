b6b80f465d174e7778be67d0eb61f2668ed396b4
jordan1995 <estivenrodriguez000@hotmaail.com>
2014-05-17 12:12:11 -0600

    ciclos

---
+++ ciclosTest/src/ciclostest/CiclosTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclostest;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author profe
+ */
+public class CiclosTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        /**
+         * ciclo for
+         */
+        int cantidad;
+        int suma = 0;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite el numero de impreciones que desea ver");
+        cantidad = Integer.parseInt(teclado.nextLine());
+
+        System.out.print("\n");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println(i);
+        }
+        System.out.print("\n");
+        System.out.println("Inverso");
+        System.out.print("\n");
+        for (int i = cantidad - 1; i > -1; i--) {
+            System.out.println(i);
+        }
+        /**
+         * ciclo while
+         */
+        while (suma <= cantidad) {
+            System.out.print(suma);
+            System.out.print(" - ");
+            suma = suma + 1;
+
+        }
+        System.out.print("\n");
+        suma = cantidad;
+        while (suma >= 0) {
+            System.out.print(suma);
+            System.out.print(" - ");
+            suma = suma - 1;
+        }
+        System.out.print("\n");
+        suma = 0;
+        do {
+            System.out.print(suma);
+            System.out.print(" - ");
+            suma = suma + 1;
+        } while (suma <= cantidad);
+    }
+
+}
